b2901a62008d1154d05c1238ea839efddd0f39a5
mayank-thapliyal-1 <111272849+mayank-thapliyal-1@users.noreply.github.com>
2023-09-19 19:46:58 +0530

    fix: type haldling

--- script.js
+++ script.js
@@ -1,38 +1,51 @@
+function timer() {
+  let time = 60;
+  const timer = document.querySelector("#timer");
+  timer.innerHTML = time;
+  setInterval(() => {
+    time--;
+    if (time === -1) time = 60;
+    timer.innerHTML = time;
+  }, 1000);
+}
+
 async function getData() {
-  const response = await fetch("https://crypto-data.onrender.com/fetchData");
-  const data = await response.json();
+  try {
+    const response = await fetch("https://crypto-data.onrender.com/fetchData");
+    const data = await response.json();
 
-  const tbody = document.querySelector("#cripto_table");
+    const tbody = document.querySelector("#cripto_table");
 
-  data.forEach((item, index) => {
-    const row = document.createElement("tr");
-    row.classList.add("table-row");
+    data.forEach((item, index) => {
+      const row = document.createElement("tr");
+      row.classList.add("table-row");
+      const last = +item.last;
+      const buy = +item.buy;
+      const sell = +item.sell;
+      const volume = +item.volume;
 
-    const difference = (((item.sell - item.buy) / item.sell) * 100).toFixed(2);
-    const savings = ((item.sell - item.buy) * item.volume).toFixed(0);
+      const difference = (((sell - buy) / sell) * 100).toFixed(2);
+      const savings = ((sell - buy) * volume).toFixed(0);
 
-    row.innerHTML = `
-    <td class="table-data">${index + 1}</td>
-    <td class="table-data">${item.name}</td>
-    <td class="table-data">₹ ${item.last.toFixed(0)}</td>
-    <td class="table-data">₹ ${item.buy.toFixed(0)} / ₹ ${item.sell.toFixed(
-      0
-    )}</td>
-    <td class="table-data ${
-      difference < 0 ? "loss" : "profit"
-    }">${difference} %</td>
-    <td class="table-data ${difference < 0 ? "loss" : "profit"}">${
-      difference < 0 ? "▼" : "▲"
-    } ₹ ${savings}</td>
-  `;
+      row.innerHTML = `
+      <td class="table-data">${index + 1}</td>
+      <td class="table-data">${item.name}</td>
+      <td class="table-data">₹ ${last.toFixed(0)}</td>
+      <td class="table-data">₹ ${buy.toFixed(0)} / ₹ ${sell.toFixed(0)}</td>
+      <td class="table-data ${
+        difference < 0 ? "loss" : "profit"
+      }">${difference} %</td>
+      <td class="table-data ${difference < 0 ? "loss" : "profit"}">${
+        difference < 0 ? "▼" : "▲"
+      } ₹ ${savings}</td>
+    `;
 
-    tbody.appendChild(row);
-  });
-  buyer(data);
+      tbody.appendChild(row);
+    });
+  } catch (error) {
+    console.log(error);
+  }
 }
-function buyer(data){
-  console.log(data);
-  
-}
 
+timer();
 getData();
